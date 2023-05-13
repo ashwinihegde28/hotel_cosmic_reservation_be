90363f958db68da0aa1c2645ce5be731aa6d0a27
Catherine Mitchell <nicholscat@yahoo.com>
2023-05-13 12:39:01 -0300

    removed seeds files

--- db/seeds/01_customers.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO customers (name, email) VALUES
-('Melody Edwards', 'melody@email.com'),
-('Christopher Thornton', 'christopher@email.com'),
-('Hayley Vasquez', 'hayley@email.com'),
-('Aron Barker', 'aron@email.com');
