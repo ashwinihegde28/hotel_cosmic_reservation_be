1b008b92ee3bdb032202aa944708ce18c500aa9e
Catherine Mitchell <nicholscat@yahoo.com>
2023-05-16 22:43:13 -0300

    fixed the seeds for services_reserved table

--- db/seeds/06_services_reserved.sql
+++ db/seeds/06_services_reserved.sql
@@ -1,5 +1,5 @@
-INSERT INTO services (name, description, image_url)
+INSERT INTO services_reserved (reservations_id, service_id)
 VALUES
-('Restaurant Galactic', 'Enjoy a dining experience of the galaxies, with the rarest finds of the universe, cooked and served to perfection', 'https://example.com/restaurant1.jpg'),
-('Shop', 'Browse our gift shop to find the more rare gifts this universe has to offer', 'https://example.com/shop1.jpg'),
-('Stellar Spa', 'Relax and rejeuvenate in our soothing space, while enjoying a variety of treatments among the cosmos', 'https://example.com/spa1.jpg');
+(5, 2),
+(6, 1),
+(7, 3);
